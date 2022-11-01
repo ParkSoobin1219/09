ded0f0bd3987c62b5caf28b1ad5aa5877a668deb
ParkSoobin1219 <psb1219j@gmail.com>
2022-11-01 16:24:57 +0900

    09_03_01

--- main.c
+++ main.c
@@ -6,19 +6,16 @@
 int main(int argc, char *argv[]) 
 {
 	int i;
-	int avg,sum;
 	int grade[SIZE];
+	int score[SIZE];
 	
-	sum=0;
+	for(i=0;i<SIZE;i++){
+		grade[i]=rand()%100;
+		score[i]=grade[i];
+	}
 	
-	for(i=0;i<5;i++){
-	    printf("5명의 점수를 입력하세요.");
-		scanf("%d", &grade[i]);
-		sum+=grade[i];
-    }
-
-	avg=sum/SIZE;
-	printf("성적 평균: %d\n", avg);
+	for(i=0;i<SIZE;i++)
+	 printf("score[%d]=%d\n", i, score[i]);
 	
 	return 0;
 }
